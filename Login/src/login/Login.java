4958a67b9d0edab7e1345a76b7117068e3eb81e2
Alejandro Alvarez <151877512+alejoprograming26@users.noreply.github.com>
2025-12-21 10:06:42 -0400

    Subiendo esta mamada

---
+++ Login/src/login/Login.java
@@ -0,0 +1,19 @@
+
+package login;
+
+import java.awt.Component;
+
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       LoginInicio LoginFrame= new LoginInicio();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+        // TODO code application logic here
+    }
+
+}
