91dc5d8d27bd13e62d8e81df42bff4aa15a88ba7
Danielings <canelondaniel47@gmail.com>
2026-02-11 16:04:18 -0400

    final proyecto 2

--- Menu_Admin/src/IGU/Menu2.java
+++ Menu_Admin/src/IGU/Menu2.java
@@ -574,11 +574,11 @@ public class Menu2 extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Cedula", "Nombre", "Apellido", "Telefono", "Direccion"
+                "Id", "Cedula", "Nombre", "Apellido", "Telefono", "Direccion"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false
+                true, false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
